36283f038094ef13c0c3937c5ebad0a3583e8a66
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-10-02 19:11:57 -0300

    excluir clientes

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,8 @@
 (function(){
-    
-})()
+    $("#tabclientes").on("click",".js-delete", function(){
+        let botaoClicado = $(this);
+        $("#btnsim")
+            .attr("data-id", botaoClicado.attr("data-id"));
+        $("#modalcliente").modal("show");
+    });
+})();
